e299189333db3338a246f30012be8a18dac7f58c
Tong Hoang Vu <tonghoangvu@outlook.com>
2022-04-08 17:53:43 +0700

    chore: update favicon

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,10 @@
 
 <p>Web demo read-vietnamese-number</p>
 <p>{NODE_ENV}</p>
+<p>
+	<a target="_blank" href="https://icons8.com/icon/FDoWrXB7G9Rc/99">99</a>
+	icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+</p>
 
 <style>
 </style>
