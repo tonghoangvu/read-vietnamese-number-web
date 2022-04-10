df117e6e00b252748de78b2141a2272f9ef24059
Tong Hoang Vu <tonghoangvu@outlook.com>
2022-04-10 22:42:31 +0700

    chore: update title on tab changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import Page from './component/Page.svelte'
 	import Footer from './component/Footer.svelte'
+
+	import * as store from './store/index'
+
+	let activeTab: string
+	store.activeTab.subscribe((value) => activeTab = value)
+
+	const TITLE_MAP = new Map([
+		['demo', 'Live demo'],
+		['option', 'Options'],
+		['about', 'About app'],
+	])
+	$: title = TITLE_MAP.get(activeTab) ?? 'Not found'
 </script>
 
 <div class="container paper">
-	<h3 class="text-secondary margin">Page title</h3>
+	<h3 class="text-secondary margin">{title}</h3>
 	<Page />
 	<hr class="margin-bottom-large" />
 	<Footer />
