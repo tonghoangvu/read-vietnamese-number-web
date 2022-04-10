86c9ae6ae75d7e744eab33f9ac7153c3847c163e
Tong Hoang Vu <tonghoangvu@outlook.com>
2022-04-10 22:14:07 +0700

    chore: add constants for icon reference

--- src/component/Footer.svelte
+++ src/component/Footer.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	const NODE_ENV: string = process.env.NODE_ENV ?? ''
+	const ICON_REF_LINK = 'https://icons8.com/icon/FDoWrXB7G9Rc/99'
+	const ICON_REF_TEXT = '99'
 </script>
 
 <footer>
 	<p>Running in <strong>{NODE_ENV}</strong> mode</p>
 	<p>
-		<a target="_blank" href="https://icons8.com/icon/FDoWrXB7G9Rc/99">99</a>
+		<a target="_blank" href="{ICON_REF_LINK}">{ICON_REF_TEXT}</a>
 		icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 	</p>
 </footer>
