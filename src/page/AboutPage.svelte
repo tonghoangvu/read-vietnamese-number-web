568ddd86f331c26bc04acc5a31868041594cc640
Tong Hoang Vu <tonghoangvu@outlook.com>
2022-04-23 23:29:46 +0700

    chore: add simple dark mode

--- src/page/AboutPage.svelte
+++ src/page/AboutPage.svelte
@@ -1,7 +1,14 @@
-<script lang="ts"></script>
+<script lang="ts">
+	function toggleDarkMode() {
+		document.querySelector('html')?.classList.toggle('dark')
+	}
+</script>
 
 <main>
 	<p>About page</p>
+	<div class="row flex-center">
+		<button on:click="{toggleDarkMode}">Toggle dark mode</button>
+	</div>
 </main>
 
 <style>
