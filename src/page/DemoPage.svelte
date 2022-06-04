f14c3dfe8b361f0711ff1fefb9230ce1c0e6c4bc
tonghoangvu <tonghoangvu@outlook.com>
2022-06-04 23:37:17 +0700

    feat: design demo page layout

--- src/page/DemoPage.svelte
+++ src/page/DemoPage.svelte
@@ -1,11 +1,56 @@
-<script lang="ts"></script>
+<script lang="ts">
+	let number: string | null = null
+
+	$: ok = number !== null && isValid(number)
+	$: message = read(number)
+
+	function isValid(number: string): boolean {
+		return !Number.isNaN(parseFloat(number))
+	}
+
+	function read(number: string | null): string {
+		if (number === null || !isValid(number)) {
+			return 'Enter a valid number'
+		}
+		return number
+	}
+</script>
 
 <main>
-	<p>Demo page</p>
+	<div class="row flex-center">
+		<div class="col">
+			<label for="num">Enter a number</label>
+			<input
+				id="num"
+				type="number"
+				placeholder="How about -3.14"
+				bind:value="{number}" />
+			<div
+				class="margin-top margin-bottom-none text-center alert"
+				class:alert-success="{ok}"
+				class:alert-warning="{!ok}">
+				{message}
+			</div>
+		</div>
+	</div>
+	<div class="row flex-center">
+		<div class="col">
+			<button disabled class="btn-secondary">Add to favorite</button>
+			<button disabled>Clear all</button>
+		</div>
+	</div>
 </main>
 
 <style>
-	p {
-		text-align: center;
+	.row {
+		margin-bottom: unset;
+	}
+
+	main input {
+		display: unset;
+	}
+
+	label {
+		padding-left: unset;
 	}
 </style>
