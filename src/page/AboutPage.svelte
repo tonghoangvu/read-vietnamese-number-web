149a9dbb638542f52b0e104cd5ee28e5048a97d1
tonghoangvu <tonghoangvu@outlook.com>
2022-06-06 13:42:50 +0700

    feat: design about page

--- src/page/AboutPage.svelte
+++ src/page/AboutPage.svelte
@@ -5,8 +5,47 @@
 </script>
 
 <main>
-	<p class="text-center">About page</p>
 	<div class="row flex-center">
-		<button on:click="{toggleDarkMode}">Toggle dark mode</button>
+		<div class="col text-center">
+			<figure class="margin-none">
+				<img src="/favicon.png" alt="Read Vietnamese Number logo" />
+				<figcaption class="margin-top-small">Version 0.0</figcaption>
+			</figure>
+			<h4 class="margin-top margin-bottom">Read Vietnamese Number</h4>
+			<p>
+				A library converts number to text in Vietnamese<br />
+				Support JavaScript and TypeScript
+			</p>
+			<div class="margin-top">
+				<a
+					href="https://www.npmjs.com/package/read-vietnamese-number"
+					target="_blank">
+					Published on NPM
+				</a>
+			</div>
+			<div class="margin-top">
+				<a
+					href="https://github.com/tonghoangvu/read-vietnamese-number"
+					target="_blank">
+					Checkout GitHub repository
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row flex-center">
+		<div class="col">
+			<button on:click="{toggleDarkMode}">Toggle dark mode</button>
+		</div>
 	</div>
 </main>
+
+<style>
+	.row {
+		margin-bottom: unset;
+	}
+
+	img {
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
